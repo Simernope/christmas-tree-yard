41a9f02fa7e9aa6c78ea3a5397531ad6341d4847
Simernope <simeon02908@gmail.com>
2023-09-09 18:14:30 +0500

    fixing bot

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,13 +7,18 @@ type OrderType = {
 
 export const sendOrder = (data: OrderType, setIsResponseOk: (value: boolean) => void) => {
     const token = '6497558141:AAHljoBZU0zFrmlVFfgwDetVsbYvTtRARt8'
-    const chatsId = ['455910348',] // '5952659463'
+    const chatsId = ['455910348', '802078730', '5952659463'] // '5952659463'
     const htmlLayout = `*[NEW] Заявка*%0AТовар - _${data.tree}_%0AЗаказчик - _${data.name}_%0AТелефон - _${data.phoneNumber}_`
 
     if (htmlLayout)
         chatsId.map(chatId => {
             fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${htmlLayout}&parse_mode=Markdown`)
-                .then(() => setIsResponseOk(true))
+                .then((response) => {
+                    response.ok
+                        ? setIsResponseOk(true)
+                        : setIsResponseOk(false)
+
+                })
                 .catch(() => setIsResponseOk(false))
         })
 }
--- src/screens/goodsScreen/Form.tsx
+++ src/screens/goodsScreen/Form.tsx
@@ -4,7 +4,7 @@ import {Button} from "../../components";
 import {sendOrder} from "../../api";
 
 const Form = (props: { tree: TreeCardProperties }) => {
-    const [isResponseOk, setIsResponseOk] = useState<boolean | undefined >()
+    const [isResponseOk, setIsResponseOk] = useState<boolean | undefined>()
     const [pending, setPending] = useState(false)
     const [phoneNumber, setPhoneNumber] = useState('')
     const [name, setName] = useState<string>('')
@@ -29,83 +29,106 @@ const Form = (props: { tree: TreeCardProperties }) => {
 
 
     useEffect(() => {
-        if (isResponseOk !== null){
+        if (isResponseOk !== undefined) {
             setPending(false)
         }
-    }, [sendOrder, isResponseOk])
+    }, [isResponseOk])
 
     return (
         <div className='flex flex-col gap-3'>
-
-            <div className='font-medium mt-5'>Оставьте заявку и мы свяжемся с вами!</div>
             {
-                isResponseOk
-                    ? <div className='font-bold text-green-900'>Все ок!</div>
-                    : isResponseOk !== undefined && <div className='font-bold text-red-500'>Ошибка</div>
-            }
-            <form className='flex flex-col gap-3' onSubmit={handleFormData}>
-                <div>
-                    <label className="block text-sm font-medium mb-2" htmlFor="username">
-                        Модель
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
-                        disabled
-                        value={props.tree.title}
-                    />
-                </div>
-                <div>
-                    <label className="block text-sm font-medium mb-2" htmlFor="username">
-                        Ваше имя
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" type="text" placeholder="Как вас зовут?"
-                        value={name}
-                        onChange={(e) => {
-                            setNameError(false)
-                            setName(e.target.value)
-                        }}
-                    />
-                    {
-                        nameError &&
-                        <p className="text-red-500 text-xs italic">Пожалуйста, введите имя.</p>
-                    }
-                </div>
-                <div>
-                    <label className="block text-sm font-medium mb-2" htmlFor="phoneNumber">
-                        Ваш номер телефона
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="phoneNumber" type="text" placeholder="Укажите номер телефона"
-                        value={phoneNumber}
-                        onChange={(e) => {
-                            setPhoneNumberError(false)
-                            setPhoneNumber(e.target.value)
-                        }
-                        }
-                    />
-                    {
-                        phoneNumberError &&
-                        <p className="text-red-500 text-xs italic">Пожалуйста, введите номер телефона.</p>
-                    }
-                </div>
-                <div className='mt-2'>
-                    <Button buttonType={'gradient'}>
-                        {
-                            pending ?
-                                <div className='flex gap-2 items-center justify-center'>
-                                    <span className="material-symbols-outlined animate-spin">autorenew</span>
-                                    <span>Загрузка...</span>
-                                </div>
-                                :
-                                <div>Оставить заявку</div>
-                        }
+                !isResponseOk &&
+                <>
 
-                    </Button>
+                    <div className='font-medium mt-5'>Оставьте заявку и мы свяжемся с вами!</div>
+                    {
+                        isResponseOk === false &&
+                        <div className='text-red-500'>Ошибка! Попробуйте снова.
+                        </div>
+                    }
+                    <form className='flex flex-col gap-3' onSubmit={handleFormData}>
+                        <div>
+                            <label className="block text-sm font-medium mb-2" htmlFor="username">
+                                Модель
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+                                disabled
+                                value={props.tree.title}
+                            />
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium mb-2" htmlFor="username">
+                                Ваше имя
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="username" type="text" placeholder="Как вас зовут?"
+                                value={name}
+                                onChange={(e) => {
+                                    setNameError(false)
+                                    setName(e.target.value)
+                                }}
+                            />
+                            {
+                                nameError &&
+                                <p className="text-red-500 text-xs italic">Пожалуйста, введите имя.</p>
+                            }
+                        </div>
+                        <div>
+                            <label className="block text-sm font-medium mb-2" htmlFor="phoneNumber">
+                                Ваш номер телефона
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="phoneNumber" type="text" placeholder="Укажите номер телефона"
+                                value={phoneNumber}
+                                onChange={(e) => {
+                                    setPhoneNumberError(false)
+                                    setPhoneNumber(e.target.value)
+                                }
+                                }
+                            />
+                            {
+                                phoneNumberError &&
+                                <p className="text-red-500 text-xs italic">Пожалуйста, введите номер телефона.</p>
+                            }
+                        </div>
+                        <div className='mt-2 sticky bottom-0'>
+                            <Button buttonType={'gradient'}>
+                                {
+                                    pending ?
+                                        <div className='flex gap-2 items-center justify-center'>
+                                            <span className="material-symbols-outlined animate-spin">autorenew</span>
+                                            <span>Загрузка...</span>
+                                        </div>
+                                        :
+                                        <div>Оставить заявку - <span> {props.tree.cost} ₽</span>
+                                        </div>
+                                }
+
+                            </Button>
+                        </div>
+                    </form>
+                </>
+            }
+            {
+                isResponseOk === true &&
+                <div className='flex flex-col gap-3 mt-5'>
+                    <div className='text-xl font-medium'>Отлично!</div>
+                    <div> Заявка на
+                        <span className='font-medium'> {props.tree.title} </span>
+                        стоимостью <span className='font-medium'> {props.tree.cost} ₽ </span>
+                        оставлена
+                    </div>
+
+                    <div>Скоро мы свяжемся с вами по номеру
+                        <span className='font-medium'> {phoneNumber}</span>
+                    </div>
+                    <div>По всем вопросам звоните: <span className='font-medium'> +7-800-555-35-35</span></div>
                 </div>
-            </form>
+            }
+
         </div>
     )
 }
